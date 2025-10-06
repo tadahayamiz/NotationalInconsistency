db71b86c6efc819818f3b795bef9d356d6c906f1
tadahayamiz <tadahaya@gmail.com>
2025-10-06 13:40:11 +0900

    update __init__

--- src/notate/__init__.py
+++ src/notate/__init__.py
@@ -5,26 +5,4 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0-dev" # fallback if package is not installed
 
-# Import main components
-from .core import *
-from .training import *
-from .data import *
-from .modules import *
-from .tools import *
-
-# For backward compatibility, expose Model at top level
-from .core import Model
-
-__all__ = [
-    # Core
-    'Model', 'module_type2class',
-    
-    # Data
-    'get_dataloader', 'get_dataset', 'get_accumulator',
-    
-    # Training  
-    'get_optimizer', 'get_scheduler', 'get_metric', 'get_process', 'get_hook',
-    
-    # Tools
-    'default_logger', 'load_config2', 'make_result_dir',
-]
+__all__ = []
